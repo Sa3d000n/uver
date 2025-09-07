7a89218927b87a00438fa7a6a5288cd906ebb817
Sa3d000n <saad_nsr@hotmail.com>
2025-09-07 21:01:00 +0300

    remove duplicate StepsSection component from Home

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,6 @@ export default function Home() {
           <HeadSection />
           <ComprehensiveSection />
           <StepsSection />
-          <StepsSection />
         </SmoothScrollWrapper>
       </main>
     </>
